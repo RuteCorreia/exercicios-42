72c861729562be280e87b73f4f379d9815f503b1
Rute Correia De Assis Conceicao <rute1107@gmail.com>
2024-08-09 12:29:50 -0300

    Subir arquivo

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def add_one(x):
+    return x + 1
+
+my_variable = 10
+
+print("Initial value of my_variable:", my_variable)
+
+my_variable = add_one(my_variable)
+
+print("Value of my_variable after add_one call:", my_variable)
