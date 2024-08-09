8eaa554c0f4c4eccd00022b4eba5e09daf70b001
Rute Correia De Assis Conceicao <rute1107@gmail.com>
2024-08-09 12:17:29 -0300

    teste

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,15 +1,14 @@
+#!/usr/bin/env python3
 import sys
 
 def shrink(s):
-    """Displays the first eight characters of the string."""
     print(s[:8])
 
 def enlarge(s):
-    """Appends 'Z' characters to make the string a total of eight characters and displays it."""
     print(s.ljust(8, 'Z'))
 
 def main():
-    # Check if there are command-line arguments
+   
     if len(sys.argv) < 2:
         print("none")
         return
